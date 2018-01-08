cb818b3ddbad9b8b05d0151bfa38699bf0c79be2
Brennan Butler <bbutler522@gmail.com>
2018-01-08 17:29:34 +0000

    Add edit, update functionality and partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
   
+  get 'signup' => 'users#new'
+  resources :users, except: [:new]
+  
   resources :articles
 end
